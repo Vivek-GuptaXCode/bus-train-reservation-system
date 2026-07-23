d8d8c6b4bdd703d29727047a78385f6fe045f762
coddfishhh <shildebjit81@gmail.com>
2026-07-23 16:13:56 +0530

    Delete server/src/db/test.js

--- server/src/db/test.js
+++
@@ -1,8 +0,0 @@
-const pool = require("./db/pool");
-
-async function test() {
-  const result = await.pool.query("SELECT 1;");
-  console.log(result.rows);
-  await.pool.end();
-}
-test().catch(console.error);
